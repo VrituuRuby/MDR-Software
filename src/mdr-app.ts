f82b2f6fc9702ee3aabd32e2d65f0c742de1fd5b
VrituuRuby <victor.velozo@outlook.com>
2025-04-01 01:59:05 -0300

    feat: crt filter + click and drag + fade in

--- src/mdr-app.ts
+++ src/mdr-app.ts
@@ -1,42 +1,148 @@
-import { Application } from "pixi.js";
+import { AdvancedBloomFilter, CRTFilter, PixelateFilter } from "pixi-filters";
+import { Application, Container, Graphics, Ticker } from "pixi.js";
 import { MDRNumber } from "./mdr-number";
 
 export const globalMousePosition = { x: 0, y: 0 };
+export let isDragging: boolean = false;
 
 export class MDRApp {
   app: Application;
+  gridContainer: Container;
+  private navigationSpeed = 10;
+  private backgroundWidth = 100 * 80 + 80;
+  private backgroundHeight = 100 * 80 + 80;
+  private crtFilter: CRTFilter;
 
   constructor() {
     this.app = new Application();
+    this.gridContainer = new Container();
+    this.app.stage.addChild(this.gridContainer);
+    this.crtFilter = new CRTFilter({
+      curvature: 3, // Distorção das bordas
+      lineWidth: 3, // Espessura das scanlines
+      lineContrast: 0.3, // Intensidade das scanlines
+      noiseSize: 3,
+      vignetting: 0.4, // Escurece as bordas (vinheta)
+    });
     this.init();
   }
 
   private async init() {
-    await this.app.init({ background: 0x101018, resizeTo: window });
+    await this.app.init({ background: "0x000000", resizeTo: window });
     document.body.appendChild(this.app.canvas);
 
-    this.trackMouse();
+    this.createBackground();
+    this.setupMouseTracking();
     this.createNumbers();
+
+    this.setupKeyboardNavigation();
+
+    this.applyCRTFilter();
+    this.startCRTAnimation();
   }
 
-  private trackMouse() {
-    window.addEventListener("mousemove", (e) => {
-      globalMousePosition.x = e.clientX;
-      globalMousePosition.y = e.clientY;
+  private createBackground() {
+    const background = new Graphics();
+    background
+      .rect(0, 0, this.app.screen.width, this.app.screen.height)
+      .fill({ color: 0x101018, alpha: 1 });
+    this.app.stage.addChildAt(background, 0);
+  }
+
+  private setupMouseTracking() {
+    this.gridContainer.interactive = true;
+    this.gridContainer.eventMode = "static";
+
+    const background = new Graphics();
+    background
+      .rect(-80, -80, this.backgroundWidth, this.backgroundHeight)
+      .fill({
+        color: 0xff0000,
+        alpha: 0.0,
+      });
+    background.interactive = true;
+    this.gridContainer.addChild(background);
+
+    this.gridContainer.on("pointermove", (e) => {
+      let localPosition = e.getLocalPosition(this.gridContainer);
+      globalMousePosition.x = localPosition.x;
+      globalMousePosition.y = localPosition.y;
+    });
+
+    window.addEventListener("pointerdown", () => {
+      isDragging = true;
+    });
+
+    window.addEventListener("pointerup", () => {
+      isDragging = false;
     });
   }
+
+  private applyCRTFilter() {
+    const pixelateFilter = new PixelateFilter(3);
+    const bloomFilter = new AdvancedBloomFilter({
+      bloomScale: 1,
+    });
+
+    this.app.stage.filters = [pixelateFilter, this.crtFilter, bloomFilter];
+  }
+
+  private startCRTAnimation() {
+    Ticker.shared.add((delta) => {
+      this.crtFilter.time += delta.deltaTime * 0.25; // Pequeno incremento para dar efeito animado
+    });
+  }
+
+  private setupKeyboardNavigation() {
+    window.addEventListener("keydown", (e) => {
+      if (e.key === "ArrowLeft") {
+        this.gridContainer.x += this.navigationSpeed;
+      }
+      if (e.key === "ArrowRight") {
+        this.gridContainer.x -= this.navigationSpeed;
+      }
+      if (e.key === "ArrowUp") {
+        this.gridContainer.y += this.navigationSpeed;
+      }
+      if (e.key === "ArrowDown") {
+        this.gridContainer.y -= this.navigationSpeed;
+      }
+
+      this.clampMovement();
+    });
+  }
+
+  private clampMovement() {
+    if (this.gridContainer.x > 90) {
+      this.gridContainer.x = 90;
+    }
+    if (this.gridContainer.x < -this.backgroundWidth + window.innerWidth) {
+      this.gridContainer.x = -this.backgroundWidth + window.innerWidth; // Não deixar mover para a esquerda além do limite
+    }
+
+    // Limitar a posição de y dentro dos limites
+    if (this.gridContainer.y > 90) {
+      this.gridContainer.y = 90; // Não deixar mover para baixo além do limite
+    }
+    if (this.gridContainer.y < -this.backgroundHeight + window.innerHeight) {
+      this.gridContainer.y = -this.backgroundHeight + window.innerHeight; // Não deixar mover para cima além do limite
+    }
+  }
+
   private createNumbers() {
-    const GRID_SIZE = 2;
-    const GAP = 64;
-    for (let i = 0; i < 16 * GRID_SIZE; i++) {
-      for (let j = 0; j < 9 * GRID_SIZE; j++) {
+    const GAP = 80;
+    for (let i = 0; i < 100; i++) {
+      for (let j = 0; j < 100; j++) {
         const value = Math.floor(Math.random() * 10);
         const x = i * GAP;
         const y = j * GAP;
         const number = new MDRNumber(value, x, y);
 
-        this.app.stage.addChild(number);
+        this.gridContainer.addChild(number);
       }
     }
+
+    this.gridContainer.x = -this.gridContainer.width / 2;
+    this.gridContainer.y = -this.gridContainer.height / 2;
   }
 }
--- src/mdr-number.ts
+++ src/mdr-number.ts
@@ -1,6 +1,6 @@
 import { Text, Ticker } from "pixi.js";
 import { lerp } from "./common";
-import { globalMousePosition } from "./mdr-app";
+import { globalMousePosition, isDragging } from "./mdr-app";
 
 export class MDRNumber extends Text {
   value: number;
@@ -12,13 +12,16 @@ export class MDRNumber extends Text {
   private direction: "x" | "y" | "none";
   private speed: number;
   private elapsed: number = 0;
+  private distance = Math.random() * 10 + 5;
+  private fadeDelay: number;
 
   constructor(value: number, x: number, y: number) {
     super({
       text: value.toString(),
-      style: { fontSize: 48, fill: 0x8fb7d7 },
+      style: { fontSize: 48, fill: 0x8fb7d7, fontFamily: "Lato" },
     });
 
+    this.alpha = 0;
     this.value = value;
     this.baseX = x;
     this.baseY = y;
@@ -29,12 +32,19 @@ export class MDRNumber extends Text {
     this.direction = this.randomizeDirection();
     this.speed = this.randomizeSpeed();
     this.anchor.set(0.5);
-    Ticker.shared.add(this.float, this);
+    this.fadeDelay = Math.random() * 4;
 
     this.eventMode = "static";
     this.cursor = "pointer";
 
     this.on("pointerdown", this.onClick);
+    this.on("pointerover", this.onMouseHover);
+
+    Ticker.shared.add(this.update, this);
+  }
+
+  private onMouseHover() {
+    isDragging ? this.onClick() : null;
   }
 
   private onClick() {
@@ -51,12 +61,24 @@ export class MDRNumber extends Text {
 
   private randomizeDirection(): "none" | "x" | "y" {
     const random = Math.random();
-    if (random < 0.7) {
+    if (random < 0.5) {
       return "none";
-    } else if (random < 0.85) {
+    } else if (random < 0.75) {
       return "x";
     } else {
       return "y";
+    }
+  }
+
+  private update(ticker: Ticker) {
+    this.fadeIn(ticker.deltaTime);
+    this.float(ticker);
+    this.updateScale();
+  }
+
+  private fadeIn(deltaTime: number) {
+    if (this.elapsed >= this.fadeDelay) {
+      this.alpha = lerp(this.alpha, 1, deltaTime * 0.1);
     }
   }
 
@@ -64,47 +86,28 @@ export class MDRNumber extends Text {
     const deltaTime = ticker.deltaTime;
     this.elapsed += deltaTime * this.speed;
 
-    const DISTANCE = 12;
-    const EPSILON = 0.1;
-
     if (this.direction === "x") {
       const offsetX = Math.cos(this.elapsed);
-      if (Math.abs(offsetX) < EPSILON) {
-        const random = Math.random();
-        if (random < 0.1) {
-          this.direction = "y";
-          this.elapsed += Math.PI / 2;
-        }
-      } else {
-        this.desiredX = this.baseX + offsetX * DISTANCE;
-      }
+      this.desiredX = this.baseX + offsetX * this.distance;
     } else if (this.direction === "y") {
       const offsetY = Math.sin(this.elapsed);
-      if (Math.abs(offsetY) < EPSILON) {
-        const random = Math.random();
-        if (random < 0.1) {
-          this.direction = "x";
-          this.elapsed += Math.PI / 2;
-        }
-      } else {
-        this.desiredY = this.baseY + offsetY * DISTANCE;
-      }
+      this.desiredY = this.baseY + offsetY * this.distance;
     }
 
-    this.x = lerp(this.x, this.desiredX, 0.1);
-    this.y = lerp(this.y, this.desiredY, 0.1);
-    this.updateScale(deltaTime);
+    this.x = lerp(this.x, this.desiredX, 0.1 * deltaTime);
+    this.y = lerp(this.y, this.desiredY, 0.1 * deltaTime);
+    this.updateScale();
   };
 
-  private updateScale(deltaTime: number) {
+  private updateScale() {
     if (this.isSelected) return;
     const distance = Math.hypot(
-      this.x - globalMousePosition.x,
-      this.y - globalMousePosition.y
+      this.baseX - globalMousePosition.x,
+      this.baseY - globalMousePosition.y
     );
 
-    const MAX_DISTANCE = 200;
-    const MAX_SCALE = 1.2;
+    const MAX_DISTANCE = 100;
+    const MAX_SCALE = 1.5;
     const MIN_SCALE = 0.8;
 
     const scaleFactor =
